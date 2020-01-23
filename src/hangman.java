d64df56aa51dfa91360dfb44f1b63d2dc2116990
Anton Jonsson <anton.jonsson8@learnet.se>
2020-01-23 10:27:14 +0100

    Initial commit

---
+++ src/hangman.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class hangman {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer();
+        StringBuffer buffer = new StringBuffer();
+        String wordToGuess;
+        int wordlength;
+        int wordtoguesslength;
+        int position;
+        int triesleft = 0;
+        int totaltries = 7;
+        int lettersremaining;
+        boolean guessinword;
+        char guess;
+        StringBuffer prevGuessedLetters;
+
+
+
+    }
+}
